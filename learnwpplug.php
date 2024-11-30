285c91754b42404d1e94548d65b3fe4941e4fccc
Jesús Yesares García <web@jesusyesares.com>
2024-11-30 16:56:49 +0100

    Moved admin parts

--- learnwpplug.php
+++ learnwpplug.php
@@ -55,36 +55,8 @@ require_once MY_PLUGIN_DIR . 'includes/post-types.php';
 
 
 
-/**
- * Show the subscriber entries per course in the admin column.
- * Get the number of subscriber entries per course and display it in the admin column.
- */
-add_filter('manage_course_posts_columns', 'course_subscribe_column');
-function course_subscribe_column($columns) {
-    $columns['subscribers'] = 'Subscribers';
-    return $columns;
-}
 
-/**
- * Display the subscriber entries per course in the admin column.
- * Get the number of subscriber entries per course and display it in the admin column.
- */
-add_action('manage_course_posts_custom_column', 'course_subscribe_column_content', 10, 2);
-function course_subscribe_column_content($column, $post_id) {
-    if ($column === 'subscribers') {
-        $subscribers = get_posts(array(
-            'post_type' => 'subscriber',
-            'meta_query' => array(
-                array(
-                    'key' => 'course_id',
-                    'value' => $post_id,
-                    'compare' => '=',
-                )
-            )
-        ));
-        echo count($subscribers);
-    }
-}
+
 
 
 
